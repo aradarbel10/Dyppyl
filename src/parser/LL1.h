243f067c60cb20e8856d5286e59635f4229901e6
Arad Arbel <aradarbel10@gmail.com>
2021-10-17 19:05:55 +0300

    regular backup

--- src/parser/LL1.h
+++ src/parser/LL1.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "Grammar.h"
-#include "Token.h"
-#include "ParseTree.h"
-#include "Tokenizer.h"
+#include "../Grammar.h"
+#include "../tokenizer/Token.h"
+#include "../ParseTree.h"
+#include "../tokenizer/Tokenizer.h"
 
 #include <unordered_map>
 #include <algorithm>
@@ -14,8 +14,12 @@ namespace dpl{
 	class LL1 {
 	public:
 
+		using accept_action = std::monostate;
+		using terminal_type = Terminal;
+		using nonterminal_type = std::string_view;
+
 		using out_type = std::variant<Token, std::pair<std::string_view, int>>;
-		
+		using symbol_type = std::variant<std::monostate, terminal_type>;		
 
 		LL1(Grammar& g, ParseTree& pt, Tokenizer& inp) : input(inp), grammar(g), out_tree(pt) {
 			grammar.initialize();
@@ -42,20 +46,20 @@ namespace dpl{
 		void operator<<(const Token& t) {
 			bool terminal_eliminated = false;
 			do {
-				if (const auto* nontr = std::get_if<std::string_view>(&parse_stack.back())) {
+				if (const auto* nontr = std::get_if<nonterminal_type>(&parse_stack.back())) {
 					if (hasEntry(t, *nontr)) {
 
-						auto& rule = grammar[*nontr][table[getTerminalType(t)][*nontr]];
+						auto& rule = grammar[*nontr][table[t][*nontr]];
 
-						std::cout << "Production out: (" << *nontr << ", " << table[getTerminalType(t)][*nontr] << ")\n";
-						out_tree << std::make_pair(*nontr, table[getTerminalType(t)][*nontr]);
+						std::cout << "Production out: (" << *nontr << ", " << table[t][*nontr] << ")\n";
+						out_tree << std::make_pair(*nontr, table[t][*nontr]);
 
 						parse_stack.pop_back();
 
 						std::for_each(rule.rbegin(), rule.rend(), [&](const auto& e) {
-							if (const auto* v = std::get_if<Token>(&e)) {
+							if (const auto* v = std::get_if<terminal_type>(&e)) {
 								parse_stack.push_back(*v);
-							} else if (const auto* v = std::get_if<std::string_view>(&e)) {
+							} else if (const auto* v = std::get_if<nonterminal_type>(&e)) {
 								parse_stack.push_back(*v);
 							}
 						});
@@ -66,8 +70,8 @@ namespace dpl{
 					}
 				}
 
-				if (const auto* tr = std::get_if<Token>(&parse_stack.back())) {
-					if (getTerminalType(*tr) == getTerminalType(t)) {
+				if (const auto* tr = std::get_if<terminal_type>(&parse_stack.back())) {
+					if (*tr == t) {
 
 						std::cout << "Token out: " << t.stringify() << '\n';
 						out_tree << t;
@@ -85,15 +89,15 @@ namespace dpl{
 		}
 
 		void printParseTable() {
-			std::cout << "\n\nParse Table:\n";
-			for (const auto& [tkn, row] : table) {
-				std::cout << dpl::log::streamer{ tkn } << " :   ";
-				for (const auto& [key, rule] : row) {
-					std::cout << key << " (" << rule << ")  ";
-				}
-				std::cout << '\n';
-			}
-			std::cout << "\n\n";
+			//std::cout << "\n\nParse Table:\n";
+			//for (const auto& [tkn, row] : table) {
+			//	std::cout << tkn << " :   ";
+			//	for (const auto& [key, rule] : row) {
+			//		std::cout << key << " (" << rule << ")  ";
+			//	}
+			//	std::cout << '\n';
+			//}
+			//std::cout << "\n\n";
 		}
 
 	private:
@@ -107,7 +111,7 @@ namespace dpl{
 
 					for (const auto& f : firsts_of_def) {
 						try {
-							if (const auto* t = std::get_if<Token>(&f)) {
+							if (const auto* t = std::get_if<terminal_type>(&f)) {
 								addEntry(*t, name, i);
 							} else {
 								std::for_each(grammar.follows[name].begin(), grammar.follows[name].end(), [&](const auto& e) {
@@ -123,11 +127,11 @@ namespace dpl{
 			}
 		}
 
-		bool hasEntry(const Token& tkn, std::string_view nontr) {
-			return table.contains(getTerminalType(tkn)) && table[getTerminalType(tkn)].contains(nontr);
+		bool hasEntry(const terminal_type& tkn, nonterminal_type nontr) {
+			return table.contains(tkn) && table[tkn].contains(nontr);
 		}
 
-		bool addEntry(const Token& tkn, std::string_view name, int i) {
+		bool addEntry(const terminal_type& tkn, nonterminal_type name, int i) {
 			if (hasEntry(tkn, name)) throw std::invalid_argument("non LL(1) grammar");
 			table[tkn][name] = i;
 		}
@@ -140,11 +144,12 @@ namespace dpl{
 
 		Grammar& grammar;
 
-		std::unordered_map<std::variant<std::monostate, Token>, std::unordered_map<std::string_view, int>> table;
+		std::unordered_map<symbol_type, std::unordered_map<nonterminal_type, int>> table;
 
 		// #TASK : use list for debug, stack for release
-		std::list<std::variant<std::string_view, Token>> parse_stack;
+		std::list<std::variant<terminal_type, nonterminal_type>> parse_stack;
 
+		// #TASK : use tree builder in LL1
 		ParseTree& out_tree;
 	};
 }
