cca6f2339f7a061b55b02e4070449b075cc8209e
Arad Arbel <aradarbel10@gmail.com>
2021-12-10 22:24:56 +0200

    added one-include header

---
+++ src/dyppyl.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Grammar.h"
+#include "ParseTree.h"
+#include "TextStream.h"
+
+#include "parser/LL1.h"
+#include "parser/LR1.h"
+
+#include "tokenizer/Token.h"
+#include "tokenizer/Tokenizer.h"
